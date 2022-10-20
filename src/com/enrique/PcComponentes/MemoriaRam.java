5dca432a6638e2132e99e0c1369e8259d8dfe5f6
enriiquee <kikiarenas46@gmail.com>
2022-10-20 18:11:19 +0200

    20-octubre

---
+++ src/com/enrique/PcComponentes/MemoriaRam.java
@@ -0,0 +1,76 @@
+package com.enrique.PcComponentes;
+
+/**
+ * Clase Placa_Base
+ */
+public class MemoriaRam {
+    /**
+     * Atributos, propiedades, variables
+     */
+    private Integer id;
+
+    private String nombre;
+    private String modelo;
+    private String marca;
+    private String frecuencia;
+    private String capacidad;
+    private Integer precio;
+
+    /**
+     * Metodos, funciones, comportamiento
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo){this.modelo = modelo;}
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca= marca;
+    }
+
+    public String getFrecuencia(){
+        return frecuencia;
+    }
+
+    public void setFrecuencia(String frecuencia){
+        this.frecuencia= frecuencia;
+    }
+
+    public String getCapacidad(){
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad){
+        this.capacidad= capacidad;
+    }
+
+    public Integer getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(Integer precio){
+        this.precio = precio;
+    }
+}
